5c8de9e42b970948e229ed9d2a6831db274f2ea6
greysonfang <greysonfang@tencent.com>
2023-01-11 11:43:01 +0800

    test

---
+++ src/main/java/com/jenkins/test/Application.java
@@ -0,0 +1,12 @@
+package com.jenkins.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Application {
+    @GetMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+}

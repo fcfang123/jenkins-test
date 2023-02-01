070cc6bd6f06a7ce61e45025b3bb4bcf0aa68f8b
greysonfang <greysonfang@tencent.com>
2023-02-01 10:23:18 +0800

    测试

--- src/main/java/com/jenkins/test/Application.java
+++ src/main/java/com/jenkins/test/Application.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
     @GetMapping("/")
     public String home() {
-        return "test11";
+        return "test112";
     }
 }
